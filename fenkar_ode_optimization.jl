bec1437134535bd40724c4a3dc138a5abcf78210
Christopher Marcotte <christopher.marcotte+git@gmail.com>
2022-08-16 07:34:10 -0400

    fixes and caches

--- fenkar_ode_optimization.jl
+++ fenkar_ode_optimization.jl
@@ -2,12 +2,13 @@ include("./exportData.jl")
 include("../../Alessio_Data/loadAlessioData.jl")
 using .exportData, .loadAlessioData
 using DifferentialEquations
-using Optimization, OptimizationBBO, OptimizationNLopt
+using Optimization, OptimizationNLopt
 using Random, DelimitedFiles
 
 Random.seed!(1234)
-const fileinit=false
-const sigdigs=6
+const modelfile=true
+const stimfile=true
+const sigdigs=5
 
 using PyPlot
 plt.style.use("seaborn-paper")
@@ -44,76 +45,92 @@ function noise!(dx, x, p, t)
 	return nothing
 end
 
-# get the data from the recordings
-basePath = basepath = "../../Alessio_Data/2011-05-28_Rec78-103_Pace_Apex/"
-inds = getPossibleIndices(basepath)
-data = []
-BCLs = []
-t0s  = []
-for ind in inds, Vind in [1,2]
-	t, tmp, BCL = getExpData(ind; Vind = Vind, tInds=1:500)
-	push!(data, tmp)
-	push!(BCLs, BCL)
-	push!(t0s, t[findfirst(tmp[1,:] .> 0.5)]-t[1]-BCL/2) 
-	# this implies Istim(t1)=(IA*sin(pi*(t1-t1-BCL/2)/BCL)^500) = IA*sin(pi/2)^500
-end
-trajs = length(BCLs);
+const target = zeros(Float64, 1, 500, 62);
+const t0s = zeros(Float64, 62);
+const BCLs = zeros(Float64, 62);
+try
+	target .= reshape(readdlm("./ode/opt/target.txt"), size(target));
+	t0s .= reshape(readdlm("./ode/opt/t0s.txt"), size(t0s));
+	BCLs .= reshape(readdlm("./ode/opt/BCLs.txt"), size(BCLs));
+catch
+	# get the data from the recordings
+	basePath = basepath = "../../Alessio_Data/2011-05-28_Rec78-103_Pace_Apex/"
+	inds = getPossibleIndices(basepath)
+	data = []
+	m = 0
+	for ind in inds, Vind in [1,2]
+		m = m+1
+		t, tmp, BCL = getExpData(ind; Vind = Vind, tInds=1:500)
+		push!(data, tmp);
+		BCLs[m] = BCL;
+		t0s[m] = t[findfirst(tmp[1,:] .> 0.5)]-t[1]-BCL/2; 
+		# this implies Istim(t1)=(IA*sin(pi*(t1-t1-BCL/2)/BCL)^500) = IA*sin(pi/2)^500
+	end
 
-# define target for optimization
-const target = zeros(Float64, 1, length(data[1]), trajs); target[1,:,:] .= transpose(reduce(vcat, data));
+	# define target for optimization
+	target[1,:,:] .= transpose(reduce(vcat, data));
+	
+	# write target, t0s, and BCLs to caches
+	open("./ode/opt/target.txt", "w") do io
+		writedlm(io, target);
+	end
+	open("./ode/opt/t0s.txt", "w") do io
+		writedlm(io, t0s);
+	end
+	open("./ode/opt/BCLs.txt", "w") do io
+		writedlm(io, BCLs);
+	end
+end
 
 # make the parameters for the solution
-tspan = (0.0,length(data[1])*2.0)
-t = collect(range(tspan[1], tspan[2]; length=length(data[1])))
+tspan = (0.0,500*2.0)
+t = collect(range(tspan[1], tspan[2]; length=500));
 u0 = rand(Float64,3)
 
 # set up model parameters (moved old ones to ./old_params.txt
-
-P = zeros(Float64, 13 + 5*trajs)
-lb= zeros(Float64, 13 + 5*trajs)
-ub= zeros(Float64, 13 + 5*trajs)
+P = zeros(Float64, 13 + 5*62)
+lb= zeros(Float64, 13 + 5*62)
+ub= zeros(Float64, 13 + 5*62)
 #####		tsi,	tv1m,	tv2m,	tvp,	twm,	twp,	td,	to,	tr,	xk,	uc,	uv,	ucsi
-P[1:13] .= [	99.3526	235.527	16.8044	26.1547	194.666	236.407	0.43510	18.0096	69.1342	10.0207	0.54296	0.55190	0.38748	][1:13]
-lb[1:13].= [ 	1.0, 	1.0, 	1.0, 	1.0, 	1.0, 	1.0, 	0.05, 	1.0, 	1.0, 	9.0, 	0.10, 	0.10, 	0.10 ][1:13]
-ub[1:13].= [ 	1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1.00, 1000.0, 	1000.0,	11.0,	0.90,	0.90,	0.90 ][1:13]
+P[1:13] .= [	22.0	333.0	40.0	10.0	65.0	1000.0	0.12	12.5	25.0	10.0	0.13	0.025	0.85	][1:13]
+lb[1:13].= [ 	1.0, 	1.0, 	1.0, 	1.0, 	1.0, 	1.0, 	0.05, 	1.0, 	1.0, 	9.0, 	0.10, 	0.01, 	0.10 	][1:13]
+ub[1:13].= [ 	1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 1000.0, 0.50, 	1000.0,	1000.0,	11.0,	0.30,	0.05,	0.90 	][1:13]
 
 # initialize stimulus parameters with randomly chosen values, adapt lb/ub to fit
-for m in 1:trajs
-	P[13 + 5*(m-1) + 1] = t0s[m]
-	P[13 + 5*(m-1) + 2] = BCLs[m]
-	P[13 + 5*(m-1) + 3] = 0.159 + 0.025*randn()
-	# can set gating variable initial conditions to be optimized
-	P[13 + 5*(m-1) + 4] = rand()
-	P[13 + 5*(m-1) + 5] = rand()
+for m in 1:62
+	P[13 + 5*(m-1) + 1] = t0s[m]					# stimlus offset / phase
+	P[13 + 5*(m-1) + 2] = BCLs[m]					# stimulus period
+	P[13 + 5*(m-1) + 3] = min(max(0.0,0.159 + 0.025*randn()),0.25)	# stimulus amplitude
+	P[13 + 5*(m-1) + 4] = rand()					# v(0) ~ U[0,1] 
+	P[13 + 5*(m-1) + 5] = rand()					# w(0) ~ U[0,1]
 	
-	lb[13 + 5*(m-1) + 1] = t0s[m]-BCLs[m]/2.0
-	lb[13 + 5*(m-1) + 2] = 100.0
+	lb[13 + 5*(m-1) + 1] = t0s[m]-min(BCLs[m]/2.0,10.0)
+	lb[13 + 5*(m-1) + 2] = BCLs[m]-min(BCLs[m]*0.1,10.0)
 	lb[13 + 5*(m-1) + 3] = 0.0
-	# can set gating variable initial conditions to be optimized
 	lb[13 + 5*(m-1) + 4] = 0.0
 	lb[13 + 5*(m-1) + 5] = 0.0
 	
-	ub[13 + 5*(m-1) + 1] = t0s[m]+BCLs[m]/2.0
-	ub[13 + 5*(m-1) + 2] = 1000.0
-	ub[13 + 5*(m-1) + 3] = 0.5
-	# can set gating variable initial conditions to be optimized
+	ub[13 + 5*(m-1) + 1] = t0s[m]+min(BCLs[m]/2.0,10.0)
+	ub[13 + 5*(m-1) + 2] = BCLs[m]+min(BCLs[m]*0.1,10.0)
+	ub[13 + 5*(m-1) + 3] = 0.25
 	ub[13 + 5*(m-1) + 4] = 1.0
 	ub[13 + 5*(m-1) + 5] = 1.0
 end
-if fileinit
+if modelfile
 	# and write over from the file(s) if using those
-	P[1:13] .= transpose(readdlm("./model_params.txt"))[1:13]
-	P[14:length(P)] .= transpose(readdlm("stim_params.txt"))[:];
-	# and then check lb/ub are valid
-	li = findall(P .<= lb)
-	if !isempty(li)
-		lb[li] .= P[li]*0.9
-	end
-	ui = findall(P .>= ub)
-	if ~isempty(ui)
-		ub[ui] .= P[ui]*1.1
-	end
-
+	P[1:13] .= transpose(readdlm("./ode/opt/model_params.txt"))[1:13];
+end
+if stimfile
+	P[14:length(P)] .= transpose(readdlm("./ode/opt/stim_params.txt"))[:];
+end
+# and then check lb/ub are valid
+li = findall(P .<= lb)
+if !isempty(li)
+	lb[li] .= P[li]*0.5
+end
+ui = findall(P .>= ub)
+if ~isempty(ui)
+	ub[ui] .= P[ui]*2.0
 end
 
 # model definition using (ensemble) ODE problem
@@ -121,11 +138,11 @@ function model1(θ,ensemble)
 	prob = ODEProblem(fenkar!, u0, tspan, θ[1:16])
 	
 	function prob_func(prob, i, repeat)
-		remake(prob, u0 = [data[i][1], θ[17+5*(i-1)], θ[18+5*(i-1)]], p = θ[[1:13; (14+5*(i-1)):(16+5*(i-1))]])
+		remake(prob, u0 = [target[1,1,i], θ[17+5*(i-1)], θ[18+5*(i-1)]], p = θ[[1:13; (14+5*(i-1)):(16+5*(i-1))]])
 	end
 
 	ensemble_prob = EnsembleProblem(prob, prob_func = prob_func)
-	sim = solve(ensemble_prob, Tsit5(), ensemble, saveat = t, save_idxs=1:1, trajectories = trajs, maxiters=Int(1e8))
+	sim = solve(ensemble_prob, Tsit5(), ensemble, saveat = t, save_idxs=1:1, trajectories = 62, maxiters=Int(1e8))
 	
 end
 
@@ -137,10 +154,10 @@ function loss(θ, _p, ensemble=EnsembleSerial(); scaling::Float64=1.0)
 	sol = model1(θ,ensemble)
 	if any((s.retcode != :Success for s in sol))
 		l = Inf
-	elseif size(Array(sol)) != size(target[:,:,1:trajs])
+	elseif size(Array(sol)) != size(target[:,:,1:62])
 		print("I'm a doodoohead poopface") 
 	else
-		l = sum(abs2, (target[:,:,1:trajs].-Array(sol)).*scaling)
+		l = sum(abs2, (target[:,:,1:62].-Array(sol)).*scaling)
 	end
 	return l,sol
 end
@@ -150,9 +167,9 @@ const l1,sol1 = loss(P,nothing); # initial loss and solution set
 
 function plotFits(θ,sol; target=target)
 
-	fig, axs = plt.subplots(Int(ceil(trajs/8)),8, figsize=(dw,dw*1.05*(Int(ceil(trajs/8))/8)), 
+	fig, axs = plt.subplots(Int(ceil(62/8)),8, figsize=(dw,dw*1.05*(Int(ceil(62/8))/8)), 
 				sharex=true, sharey=true, constrained_layout=true)
-	for n in 1:trajs
+	for n in 1:62
 		# linear indexing into Array{Axis,2}
 		axs[n].plot(t, target[1,:,n], "-k", linewidth=1.6)
 		# plotting the (negated) stimulus current to make sure things line up
@@ -167,69 +184,73 @@ function plotFits(θ,sol; target=target)
 	return fig, axs
 end
 
-function saveprogress(ind,θ,l,sol)
-	open("./ode/flux/all_params.txt", "w") do io
+function saveprogress(ind,θ,l,sol; plotting=false)
+	open("./ode/opt/all_params.txt", "w") do io
 		write(io, "# Loss = $(l)\n\n")
 		write(io, "# tsi\ttv1m\ttv2m\ttvp\ttwm\ttwp\ttd\tto\ttr\txk\tuc\tuv\tucsi\n")
 		writedlm(io, transpose(round.(θ[1:13],sigdigits=sigdigs)))
 		write(io,"\n")
 		write(io, "# t0\tTI\tIA\tv0\tw0\n")
-		writedlm(io, transpose(reshape(round.(θ[14:end],sigdigits=sigdigs), 5, :)))
+		writedlm(io, transpose(reshape(round.(θ[14:end],sigdigits=sigdigs-1), 5, :)))
 	end
-	open("./model_params.txt", "w") do io
+	open("./ode/opt/model_params.txt", "w") do io
 		writedlm(io, transpose(round.(θ[1:13],sigdigits=sigdigs)))
 	end
-	open("./stim_params.txt", "w") do io
-		writedlm(io, transpose(reshape(round.(θ[14:end],sigdigits=sigdigs), 5, :)))
+	open("./ode/opt/stim_params.txt", "w") do io
+		writedlm(io, transpose(reshape(round.(θ[14:end],sigdigits=sigdigs-1), 5, :)))
 	end
-	fig, axs = plotFits(θ,sol)
-	fig.savefig("./ode/flux/all_fits.pdf",bbox_inches="tight")
-	plt.close(fig)
-	
-	plt.figure(figsize=(sw,sw),constrained_layout=true)
-	plt.loglog(iter,"-k")
-	plt.xlabel("Iteration")
-	plt.ylabel("Loss")
-	plt.savefig("./ode/flux/loss.pdf",bbox_inches="tight")
-	plt.close()
+	if plotting
+		fig, axs = plotFits(θ,sol)
+		fig.savefig("./ode/opt/all_fits.pdf",bbox_inches="tight")
+		plt.close(fig)
+		
+		plt.figure(figsize=(sw,sw),constrained_layout=true)
+		plt.loglog(iter,"-k")
+		plt.xlabel("Iteration")
+		plt.ylabel("Loss")
+		plt.savefig("./ode/opt/loss.pdf",bbox_inches="tight")
+		plt.close()
+	end
 	return nothing
 end
 
 iter = Float64[]
-cb = function (θ,l,sol) # callback function to observe training
+cb = function (θ,l,sol; plotting=false) # callback function to observe training
         push!(iter, l)
 	if isinf(l) || isnothing(l)
 	        return true
-	elseif l < l1 && mod(length(iter),200) == 0
+	elseif mod(length(iter),10) == 0
 		print("Iter = $(length(iter)), \tLoss = $(round(l;sigdigits=sigdigs)), \tReduction by $(round(100*(1-l/l1);sigdigits=sigdigs))%.\n");
-		saveprogress(length(iter),θ,l,sol);
+		if l < 1.1*maximum(iter) && mod(length(iter),50) == 0
+			saveprogress(length(iter),θ,l,sol; plotting=plotting);
+		end
 	end
         return false
 end
 
 # Optimization function:
 f = OptimizationFunction(loss, Optimization.AutoForwardDiff())
+
 # Optimization Problem definition:
 optProb = OptimizationProblem(f, P, p = SciMLBase.NullParameters(), lb=lb, ub=ub);
+
 # Optimization:
-if l1 > 1000.0
-	result = solve(optProb, NLopt.G_MLSL_LDS(), local_method = NLopt.LD_LBFGS(); maxiters=50_000, callback=cb)
-	#P .= result.u
-end
-# Optimization:
-result = solve(optProb, NLopt.LD_SLSQP(); maxiters=10_000, callback=cb)
+print("\nOptimizing with NLopt.LD_SLSQP():\n")
+result = solve(optProb, NLopt.LD_SLSQP(); callback=cb)
 
 l,sol = loss(result.u, nothing);
 @assert l ≈ result.minimum
-@assert l < l1
-saveprogress(length(iter),result.u,l,sol)
+print("\n\tFinal loss: $(l); Initial loss: $(l1).\n")
 
-open("./ode/flux/all_params.txt", "a") do io
-	write(io, "\n")
-	write(io, "# Loss (initial) = $(loss(P,nothing)[1])\n")
-	write(io, "# Loss (final) = $(loss(result.u,nothing)[1])\n")
-	Q = result.u; Q[1:13] .= P[1:13];	
-	write(io, "# Loss (resample) = $(loss(Q,nothing)[1])\n")
+if l < l1
+	saveprogress(length(iter),result.u,l,sol; plotting=true)
+	open("./ode/opt/all_params.txt", "a") do io
+		write(io, "\n")
+		write(io, "# Loss (initial) = $(loss(P,nothing)[1])\n")
+		write(io, "# Loss (final) = $(loss(result.u,nothing)[1])\n")
+		Q = result.u; Q[1:13] .= P[1:13];	
+		write(io, "# Loss (resample) = $(loss(Q,nothing)[1])\n")
+	end
+else
+	print("\n l = $(l) > $(l1) = l1.\n");
 end
-
-
